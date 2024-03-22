73d9b53f4eacedeb040af5d7a91f62e3d06fe306
Manuel Tiago Pereira <manuel@mtpereira.com>
2024-03-22 18:40:59 +0000

    Allow decks requests without a trailing slash

--- web/routes.go
+++ web/routes.go
@@ -7,5 +7,5 @@ import (
 
 func addRoutes(mux *http.ServeMux, log *slog.Logger) {
 	logRequests := newLoggerMiddleware(log)
-	mux.Handle("POST /v1/decks/", logRequests(handlePostDeck()))
+	mux.Handle("POST /v1/decks", logRequests(handlePostDeck()))
 }
