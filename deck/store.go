d098601c1a89e13a0b2e7ef5b1e88c573179ffe8
Manuel Tiago Pereira <manuel@mtpereira.com>
2024-03-23 08:08:23 +0000

    Add logging to deck store

--- deck/store.go
+++ deck/store.go
@@ -23,13 +23,18 @@ type DeckStore struct {
 }
 
 func (ds *DeckStore) Create(d Deck) {
+	ds.log.Info("store", "create", "started", "deckID", d.DeckID)
 	ds.store[d.DeckID] = &d
+	ds.log.Info("store", "create", "finished", "deckID", d.DeckID)
 }
 
 func (ds *DeckStore) QueryById(u uuid.UUID) (Deck, error) {
+	ds.log.Info("store", "query", "started", "deckID", u)
 	d := ds.store[u]
 	if d == nil {
+		ds.log.Info("store", "query", "not found", "deckID", u)
 		return Deck{}, errors.New("Deck not found")
 	}
+	ds.log.Info("store", "query", "finished", "deckID", u)
 	return *d, nil
 }
